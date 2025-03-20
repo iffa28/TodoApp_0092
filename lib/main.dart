7632306f18e069f1705c4e5b85bfb1fb99c0fa27
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-03-20 21:46:41 +0700

    memanggil class FormPage untuk memunculkan halaman form

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 
-import 'package:belajar_statefull_widget/profile_page.dart';
+import 'package:belajar_statefull_widget/form_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const ProfilePage(),
+      home: const FormPage(),
     );
   }
 }
@@ -28,14 +28,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  
-  
-  
-
-  
-  
-  
-  
 
   final String title;
 
@@ -48,50 +40,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      
-      
-      
-      
-      
+
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
-    
-    
-    
-    
-    
     return Scaffold(
       appBar: AppBar(
-        
-        
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         
         
         title: Text(widget.title),
       ),
       body: Center(
-        
-        
         child: Column(
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
